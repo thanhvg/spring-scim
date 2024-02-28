d017938fd922fb86a8049e2223fd46113084b976
Thanh <thanhvg@gmail.com>
2024-02-27 21:07:50 -0700

    spring-boot-starter-web

---
+++ src/test/java/com/pason/scim/ScimApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pason.scim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScimApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
